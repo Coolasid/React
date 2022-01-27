b867aa614f5668c8657c4d3e5777c47180a80f54
Siddesh Patil <87792589+Coolasid@users.noreply.github.com>
2022-01-27 22:52:16 +0530

    27-01

--- 1st week/1st Day WebPack/live/dist/main.js
+++
@@ -1,1 +0,0 @@
-console.log("sid"),console.log(10);
--- 1st week/1st Day WebPack/live/src/index.js
+++ 1st week/1st Day WebPack/live/src/index.js
@@ -1,13 +1,15 @@
-console.log("sid");
+import { add, mult } from "./calc";
 
-//add function
+import "./style.css"
 
-function add(a, b){
-
-    //take two arg, returns sum of both
-
-    return a + b;
-
-}
 
 console.log(add(5,5));
+console.log(mult(10,20));
+
+
+const h1 = document.createElement("h1");
+
+h1.classList.add("redText");
+h1.innerText = "hello sid"
+
+document.getElementById("root").appendChild(h1);
