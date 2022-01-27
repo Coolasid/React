1b0b0b38d8a8d7726516c83182c44b2b120e5736
Siddesh Patil <87792589+Coolasid@users.noreply.github.com>
2022-01-27 14:49:51 +0530

    27-1

---
+++ 1st week/1st Day WebPack/rec/src/index.js
@@ -0,0 +1,14 @@
+// console.log("sid");
+
+
+function sid(a, b){
+
+    return a+b;
+}
+
+function sidM(a, b){
+
+    return a*b;
+}
+
+console.log(sidM(5,5));
---
+++ 1st week/1st Day WebPack/rec/webpack.config.js
@@ -0,0 +1,16 @@
+
+// here we can config the webpack
+
+const path =  require("path")
+
+module.exports = {
+
+    entry:"./src/index.js",
+    output:{
+
+        path: path.join(__dirname, "build"),
+        filename: "bundle.js"
+
+    }
+
+}
