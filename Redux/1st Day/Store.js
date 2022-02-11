0a5975a48350392cb85459e6be22e528b1d65892
Siddesh Patil <87792589+Coolasid@users.noreply.github.com>
2022-02-11 16:39:59 +0530

    11-2

---
+++ Redux/1st Day/Store.js
@@ -0,0 +1,40 @@
+const addCount = {type: "INC_COUNT", payload: 1};
+const decCount = { type: "DEC_COUNT", payload: 1 };
+
+class Store{
+    constructor(reducer, init){
+        this.reducer = reducer;
+        this.state = init;
+    }
+
+    getState(){
+        return this.state;
+    }
+
+    dispatch(action){
+        //
+        this.state = this.reducer(this.state, action);
+    }
+}
+
+
+
+const reducer = (store, action) =>{
+    switch(action.type){
+        case "INC_COUNT":
+            return {...store, count: store.count + action.payload }
+        default:
+            return store;
+    }
+
+};
+
+//--------------------reducer init
+
+const init = { count: 0, todo: [] };
+
+const store = new Store(reducer, init);
+console.log(store.getState());
+
+store.dispatch({type: "INC_COUNT", payload:1})
+console.log(store.getState());
